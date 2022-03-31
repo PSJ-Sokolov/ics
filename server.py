96693054521876900bda72b5c8989f35c5c2285e
PSJ Sokolov <PhilipSokolov@protonmail.com>
2022-03-31 20:43:14 +0200

    Fix quotes
    Make them nicer to look at anyway.
    
    And put the docstrings at the actual proper place.
    
    The code that we got clearly is written by some kind of research scientist.

--- server.py
+++ server.py
@@ -8,33 +8,33 @@ from mesa.visualization.modules import ChartModule
 from mesa.visualization.ModularVisualization import ModularServer
 from model import SIRModel
 
-''' Portrayal function: defines the portrayal of the cells '''
 def portrayCell(cell):
+    """ Portrayal function: defines the portrayal of the cells """
     assert cell is not None
-    portrayal = {"Shape": "rect",
-                 "w": 1,
-                 "h": 1,
-                 "Filled": "true",
-                 "Layer": 1,
-                 "Color": "white"} # Default colour, used for empty cells
-    logging.debug(f'Cell at {cell.x, cell.y} with state: {cell.state}')
-    portrayal['Color'] = cell.state.value
+    portrayal = {'Shape': 'rect',
+                 'w': 1,
+                 'h': 1,
+                 'Filled': 'true',
+                 'Layer': 1,
+                 'Color': 'white'} # Default colour, used for empty cells
+    logging.debug(f"Cell at {cell.x, cell.y} with state: {cell.state}")
+    portrayal["Color"] = cell.state.value
     return portrayal
 
 
-''' Construct the simulation grid, all cells displayed as 5x5 squares '''
+""" Construct the simulation grid, all cells displayed as 5x5 squares """
 gridwidth = 100 # Change these parameters to change the grid size
 gridheight = 100
 
 # Make a grid to plot the population dynamics
 grid = CanvasGrid(portrayCell, gridwidth, gridheight, 5*gridwidth, 5*gridheight)
 # Make a chart for plotting the density of individuals
-chart = ChartModule([{"Label": "S", "Color": "grey"},{"Label": "I", "Color": "red"}], data_collector_name='datacollector1')
+chart = ChartModule([{"Label": "S", "Color": "grey"},{"Label": "I", "Color": "red"}], data_collector_name="datacollector1")
 # Let chart plot the mean infection time
-#chart = ChartModule([{"Label": "Mean_infduration", "Color": "Black"}], data_collector_name='datacollector2')
+#chart = ChartModule([{"Label": "Mean_infduration", "Color": "Black"}], data_collector_name="datacollector2")
 
 
-''' Launch the server that will run and display the model '''
+""" Launch the server that will run and display the model """
 server = ModularServer(SIRModel,
                        [grid, chart],
                        "SI-model",
