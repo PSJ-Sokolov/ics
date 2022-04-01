84c7e1ab0d358505a239d1a196ebbaa458e6e842
PSJ Sokolov <PhilipSokolov@protonmail.com>
2022-04-01 07:33:35 +0200

    Add argparse
    Now we can change parameters from the CLI (for rapid testing).

--- run.py
+++ run.py
@@ -1,4 +1,16 @@
 from server import makeServer
-server = makeServer(i = 2.0, di = 5, hi = 10)
-server.port = 8521 # Default port
-server.launch()
+import logging
+DEBUG = logging.debug
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Run model', epilog='live long and prosper!')
+    parser.add_argument('-i', action='store', type=float, default=2.0, help='Provide infectivity (2.0 by default)')
+    parser.add_argument('-d', action='store', type=int,   default=5,   help='Provide infectivity_duration (5 by default)')
+    parser.add_argument('-r', action='store', type=int,   default=10,  help='Provide h_infectivity (10 by default)')
+    args = parser.parse_args()
+    DEBUG(args)
+
+    server      = makeServer(args.i, args.d, args.r)
+    server.port = 8521 # Default port
+    server.launch()
