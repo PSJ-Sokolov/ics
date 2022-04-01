edc220ef10d96c3db0624e21e4370e48fb556834
PSJ Sokolov <PhilipSokolov@protonmail.com>
2022-04-01 08:23:33 +0200

    Change arguments and logging stuff

--- run.py
+++ run.py
@@ -1,15 +1,17 @@
 from server import makeServer
 
 import logging
-logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='Run model', epilog='live long and prosper!')
-    parser.add_argument('-i', action='store', type=float, default=2.0, help='Provide infectivity (2.0 by default)')
-    parser.add_argument('-d', action='store', type=int,   default=5,   help='Provide infectivity_duration (5 by default)')
-    parser.add_argument('-r', action='store', type=int,   default=10,  help='Provide h_infectivity (10 by default)')
+    parser.add_argument('-i', action='store', type=float, default=2.0,   help='Provide infectivity (2.0 by default)')
+    parser.add_argument('-d', action='store', type=int,   default=5,     help='Provide infectivity_duration (5 by default)')
+    parser.add_argument('-r', action='store', type=int,   default=10,    help='Provide h_infectivity (10 by default)')
     args = parser.parse_args()
+    #logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
+    #logging.basicConfig(level=logging.WARNING)
     logging.info(args)
 
     server      = makeServer(args.i, args.d, args.r)
